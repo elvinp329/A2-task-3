ebcbb17c259fe531c43ed945bb71a99b0ab61eb8
Elvinp329 <elvinkj@code.edu.az>
2023-01-04 15:16:01 +0400

    A2 task2

---
+++ A2 task3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace A2_task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            decimal n =  decimal.Parse(Console.ReadLine());
+            int real = (int)n;
+            decimal kesr = n - real;
+            decimal x = (real / 1000M);
+            decimal reverse = x + kesr * 1000;
+            
+            
+            Console.WriteLine($"{reverse}");
+        }
+    }
+}
